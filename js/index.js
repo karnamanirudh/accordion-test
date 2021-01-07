204655294439f214b0d5f67b9db1ecc84d7606f1
Anirudh V K <karnamanirudh@gmail.com>
2021-01-07 19:44:19 +0000

    Inital commit for accordion-test

---
+++ js/index.js
@@ -0,0 +1,47 @@
+var acordions = document.getElementsByClassName("accordion-header");
+var panel = document.getElementsByClassName('accordion-body');
+var sectionText = document.getElementsByClassName('s1');
+var text;
+var sections;
+
+
+window.onload = function () {
+    document.getElementById('section-list').style.display = 'none';
+};
+for (var i = 0; i < acordions.length; i++) {
+
+    acordions[i].addEventListener('click', function () {
+        var setClasses = !this.classList.contains('active');
+        var sectionIndicatorText = document.getElementById('section');
+        setClass(acordions, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        
+        sectionIndicatorText.style.display = 'block';
+        document.getElementById('section-list').style.display = 'none';
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            var displaySectionList = document.getElementById('section-list');
+            displaySectionList.style.display = "block";
+            sectionIndicatorText.style.display = 'none';
+            sections = this.innerHTML;
+            for (var j = 0; j < sectionText.length; j++) {
+                if (sectionText[j].innerHTML == sections) {
+                    sectionText[j].classList.add('selected-accordion')
+                }
+                else {
+                    sectionText[j].classList.remove('selected-accordion')
+                }
+            }
+            this.nextElementSibling.classList.toggle("show");
+        }
+    });
+}
+
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
+
